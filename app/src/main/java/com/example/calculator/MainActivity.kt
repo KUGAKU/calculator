e3f313668c8ec968eb008401e4d22273b3d480ee
gaku yamamoto <githubgemini1124@gmail.com>
2021-02-11 22:15:39 +0900

    implement operation button

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,15 +3,54 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View){
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = ""
+        lastDot = false
+        lastNumeric = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if(lastNumeric && !lastDot) {
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View) {
+        val textView = findViewById<TextView>(R.id.textView)
+        if(lastNumeric && !isOperatorAdded(textView.text.toString())) {
+            textView.append((view as Button).text)
+            lastDot = false
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean{
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+        }
     }
 }
